53b3ee84129d34f6539b3db8c998591b21f1e422
jenn kaplan <jenn.kaplan@plangrid.com>
2018-04-24 10:26:52 -0700

    more jkap changes

--- keyboards/ergodox_ez/keymaps/jkap_osx/keymap.c
+++ keyboards/ergodox_ez/keymaps/jkap_osx/keymap.c
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                               ALT_T(KC_APP),  KC_LGUI,
                                                               KC_HOME,
                                                KC_LGUI,KC_DEL,KC_END,
-        // right hand
+             // right hand
              KC_RGHT,     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
              KC_LBRC,     KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
                           KC_H,   KC_J,   KC_K,   KC_L,   LT(MDIA, KC_SCLN),KC_ENT,
